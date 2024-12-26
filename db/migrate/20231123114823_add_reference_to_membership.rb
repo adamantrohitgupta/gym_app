646c0da6cc229b15eb01c774d7efcdf526f9d679
Anshu Kumar <ansh191ky@gmail.com>
2024-12-26 11:49:20 +0530

    remove migration

--- db/migrate/20231123114823_add_reference_to_membership.rb
+++
@@ -1,7 +0,0 @@
-class AddReferenceToMembership < ActiveRecord::Migration[7.0]
-  def up
-  drop_table :memberships_users
-  end
-  def down
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_11_114333) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_21_063155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,6 +119,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_11_114333) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_participants_on_room_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
   create_table "plans", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "membership_id"
@@ -181,4 +190,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_11_114333) do
   add_foreign_key "memberships", "gyms"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "participants", "rooms"
+  add_foreign_key "participants", "users"
 end
